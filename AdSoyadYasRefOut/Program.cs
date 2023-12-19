74cd19c894dfa5ab9545d8eef3641b351dfe256f
yeliz601 <yeliz601@BPC11>
2023-12-19 10:51:52 +0300

    Proje dosyası ekle.

---
+++ AdSoyadYasRefOut/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdSoyadYasRefOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "yeliz";
+            string soyad = "bolat";
+            int yas = 30;
+            AdSoyad adSoyad = new AdSoyad();
+            adSoyad.BilgiGuncelle(ref ad, ref soyad, ref yas);
+
+
+            Console.WriteLine("Önceki Değerler:");
+            Console.WriteLine("Ad: " + ad);
+            Console.WriteLine("Soyad: " + soyad);
+            Console.WriteLine("Yaş: " + yas);
+
+        }
+    }
+    class AdSoyad
+    {
+        public void BilgiGuncelle(ref string ad, ref string soyad, ref int yas)
+        {
+            ad = "fulya";
+            soyad = "bolat";
+            yas = 18;
+        }
+    }
+}
